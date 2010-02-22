187ab7b7d6c53c91caf0b5320f252aab1a875b7e
Carlos <carlos@localhost>
2010-02-22 10:26:16 -0300

    Drop xml support

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -3,42 +3,37 @@ class SnippetsController < ApplicationController
   helper_method :can_edit?
 
   # GET /snippets
-  # GET /snippets.xml
   def index
     @snippets = Snippet.paginate :per_page => 10, :page => params[:page], :order=> 'updated_at DESC'
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @snippets }
     end
   end
 
   # GET /snippets/1
-  # GET /snippets/1.xml
   def show
     @snippet = Snippet.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @snippet }
       format.text { render :text => @snippet.code }
       format.json { render :json => @snippet.to_json }
     end
   end
 
+  # GET /snippets/1/fullscreen
   def fullscreen
     @snippet = Snippet.find(params[:id])
     render :layout => false
   end
 
   # GET /snippets/new
-  # GET /snippets/new.xml
   def new
     @snippet = Snippet.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @snippet }
     end
   end
 
@@ -48,7 +43,6 @@ class SnippetsController < ApplicationController
   end
 
   # POST /snippets
-  # POST /snippets.xml
   def create
     @snippet = Snippet.new(params[:snippet])
 
@@ -57,16 +51,13 @@ class SnippetsController < ApplicationController
         owns!(@snippet.id)
         flash[:notice] = 'Snippet was successfully created.'
         format.html { redirect_to(@snippet) }
-        format.xml  { render :xml => @snippet, :status => :created, :location => @snippet }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @snippet.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /snippets/1
-  # PUT /snippets/1.xml
   def update
     @snippet = Snippet.find(params[:id])
 
@@ -74,23 +65,19 @@ class SnippetsController < ApplicationController
       if @snippet.update_attributes(params[:snippet])
         flash[:notice] = 'Snippet was successfully updated.'
         format.html { redirect_to(@snippet) }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @snippet.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /snippets/1
-  # DELETE /snippets/1.xml
   def destroy
     @snippet = Snippet.find(params[:id])
     @snippet.destroy
 
     respond_to do |format|
       format.html { redirect_to(snippets_url) }
-      format.xml  { head :ok }
     end
   end
 
